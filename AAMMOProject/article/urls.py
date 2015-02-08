c3e227d5ce48f240e739d9a090783f80f25dde93
aya-gamal1 <eng.aya_gamal@outlook.com>
2015-02-08 15:50:19 +0200

    modified project urls.py  and create app urls.py

--- AAMMOProject/article/urls.py
+++ AAMMOProject/article/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'AAMMOProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^article/', include('article.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+
+     url(r'^createarticle/$','article.views.createarticle')
+    #url(r'^details/(?P<question_id>\d+)/$','mytest.views.details'),
 ]
