6b2ba97b38cf859f1c4b80ddc3f2676119cb19a4
Omar Sourour <omar.e.sourour@gmail.com>
2015-02-09 20:44:45 +0200

    Created registration form and main app

--- AAMMOProject/article/urls.py
+++ AAMMOProject/article/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # url(r'^$', 'AAMMOProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-     url(r'^createarticle/$','article.views.createarticle')
+     # url(r'^createarticle/$','article.views.createarticle')
     #url(r'^details/(?P<question_id>\d+)/$','mytest.views.details'),
 ]
--- AAMMOProject/users/forms.py
+++ AAMMOProject/users/forms.py
@@ -10,35 +10,36 @@ class RegisterForm(forms.Form):
 
 	# The username field for the login form.
 	user_name = forms.CharField(
-		label="Enter user name: ",
+		label="Enter user name",
 		max_length=100,
 		widget=forms.TextInput(attrs={'placeholder': 'Enter username here ...'})
 	)
 
 	# The email field for the login form.
 	user_email = forms.EmailField(
-		label="Enter your email: ",
+		label="Enter your email",
 		max_length=200,
 		widget=forms.EmailInput(attrs={'placeholder': 'Enter email here ...'})
 	)
 
 	# The password field for the login form
 	user_password = forms.CharField(
-		label="Enter your password: ",
+		label="Enter your password",
 		max_length=100,
 		widget=forms.PasswordInput(attrs={'placeholder': 'Enter password here ...'})
 	)
 
 	# The password confirmation field for the login form.
 	user_password_confirm = forms.CharField(
-		label="Enter your password: ",
+		label="Enter your password",
 		max_length=100,
 		widget=forms.PasswordInput(attrs={'placeholder': 'Re-enter password here ...'})
 	)
 
 	# The image selection field for the login form.
 	user_image = forms.ImageField(
-		label="Choose your profile picture: "
+		label="Choose your profile picture",
+		help_text="Maximum: 4 Mb"
 	)
 
 	# Create captcha field.
@@ -60,6 +61,29 @@ class RegisterForm(forms.Form):
 		# Check if the entered passwords match
 		if password1 != password2:
 			raise forms.ValidationError("Entered passwords don't match!")
+		else:
+			return password2
+
+	def clean_user_image(self):
+		"""
+		This function checks whether the image chosen is smaller than the maximum allowable size for the image.
+		"""
+
+		# The maximum allowable size for the image in bytes.
+		maximum_image_size = 4 * 1024 * 1024
+
+		# Get the chosen image element.
+		image = self.cleaned_data['user_image']
+
+		# If the image was uploaded successfully
+		if image:
+			# Make sure the image size is not larger than the maximum allowable size.
+			if image._size > maximum_image_size:
+				raise forms.ValidationError("The image exceeds maximum image size (4 Mb)!")
+			else:
+				return image
+		else:
+			raise forms.ValidationError("Could not read uploaded image! Type not supported!")
 
 
 class LoginForm(forms.Form):
