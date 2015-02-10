574f5ae9b1ac574cbfc488fc14123156f9a21441
aya-gamal1 <eng.aya_gamal@outlook.com>
2015-02-10 17:26:09 +0200

    solve conflict in article app

---
+++ AAMMOProject/AAMMOProject/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^', include('main.urls')),
+	#url(r'^captcha/', include('captcha.urls')),
+	url(r'^article/', include('article.urls')),
+	url(r'^users/', include('users.urls')),
+]
