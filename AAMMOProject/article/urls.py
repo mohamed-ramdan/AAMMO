fbcc5ade769d9d99d846b9ba9b8d58e0d676297e
AmiraAli <amira_ali_marzouk@outlook.com>
2015-02-10 15:55:32 +0200

    add home page , article page, admin page , articles page and all functions in view

--- AAMMOProject/article/urls.py
+++ AAMMOProject/article/urls.py
@@ -6,6 +6,21 @@ urlpatterns = [
     # url(r'^$', 'AAMMOProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-     url(r'^createarticle/$','article.views.createarticle')
+     url(r'^createarticle/$','article.views.createarticle'),
+
+     url(r'^home_article/$','article.views.home_article'),
+
+     url(r'^list_articles/$','article.views.list_articles'),
+
+     url(r'^admin_article/$','article.views.admin_article'),
+
+     url(r'^sort_published/$','article.views.sort_published'),
+
+     url(r'^like/(?P<article_id>\d+)/$','article.views.like'),
+
+     url(r'^unlike/(?P<article_id>\d+)/$','article.views.unlike'),
+
+     url(r'^open_article/(?P<article_id>\d+)/$','article.views.open_article')
+
     #url(r'^details/(?P<question_id>\d+)/$','mytest.views.details'),
 ]
