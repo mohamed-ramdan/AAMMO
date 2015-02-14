e0d25ce1bed3e5106a51e51dc92d344f577e5539
mohamedramadan <mramadan181291@gmail.com>
2015-02-14 18:39:25 +0200

    update check_comment_emoticons function

--- AAMMOProject/comments/polite.py
+++ AAMMOProject/comments/polite.py
@@ -13,5 +13,7 @@ def check_politness(text):
 def check_emoticons_existance(text):
 	emoticons = {':)':'/static/imgs/smile.gif',':P':'/static/imgs/tongue.gif','>:(':'/static/imgs/angry.gif',':O':'/static/imgs/shocked.gif'}
 	for face in emoticons:
-		text = text.replace(face,emoticons[face])
+		text = text.replace(face,'<img src="' + emoticons[face]+'" width="60px" height="50px" />')
 	return text
+
+# "you are kind :)" is gonna be replaced with   -->   you are kind <img src="/static/imgs/smile.gif" width="60px" height="50px" />
